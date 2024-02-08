9c4edb198fe66bbfcf2af919e6dca342301d167f
Gemma <jemmimah.abdul@decagon.dev>
2024-02-09 00:18:21 +0100

    Updated Project

--- FloBank.Core/Services/AccountService.cs
+++ FloBank.Core/Services/AccountService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FloBank.Core.IServices;
+using FloBank.Data;
+using FloBank.Model.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,42 @@ using System.Threading.Tasks;
 
 namespace FloBank.Core.Services
 {
-    public class AccountService
+    public class AccountService : IAccountService
     {
+        private readonly FloBankDbContext dbContext;
+        public Account Authenticate(string AccountName, string Pin)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Account Create(Account account, string Pin, string ConfirmPin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Account> GetAllAccounts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Account GetByAccountNumber(string AccountNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Account GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Account account, string Pin = null)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
